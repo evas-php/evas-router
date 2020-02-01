1f8d7bc00b6cad96c40ccc5085e6ec815e58f8a8
Egor Vasyakin <evasyakin@mail.ru>
2020-02-01 23:20:40 +0700

    Create RoutingResultHandleException.php

---
+++ src/Result/Exception/RoutingResultHandleException.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * @package evas-php/evas-router
+ */
+namespace Evas\Router\Result\Exception;
+
+use Evas\Router\RouterException;
+
+/**
+ * Класс исключений обработки результата роутинга.
+ * @author Egor Vasyakin <e.vasyakin@itevas.ru>
+ * @since 1.0
+ */
+class RoutingResultHandleException extends RouterException
+{}
