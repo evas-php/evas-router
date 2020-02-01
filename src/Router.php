2aeb663cb4172ba8340b5daa56b05c9bee190662
Egor Vasyakin <evasyakin@mail.ru>
2020-02-01 23:21:10 +0700

    Create Router.php

---
+++ src/Router.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * @package evas-php/evas-router
+ */
+namespace Evas\Router;
+
+use Evas\Router\Map\MapRouter;
+
+/**
+ * Общий класс роутера.
+ * @author Egor Vasyakin <e.vasyakin@itevas.ru>
+ * @since 1.0
+ */
+class Router extends MapRouter
+{}
