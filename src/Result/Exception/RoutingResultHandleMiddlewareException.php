aeb4933b6a88c6eda61c70ed7d52278a8211ffcc
Egor Vasyakin <evasyakin@mail.ru>
2020-02-01 23:20:51 +0700

    Create RoutingResultHandleMiddlewareException.php

---
+++ src/Result/Exception/RoutingResultHandleMiddlewareException.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * @package evas-php/evas-router
+ */
+namespace Evas\Router\Result\Exception;
+
+use Evas\Router\Result\Exception\RoutingResultHandleException;
+
+/**
+ * Класс исключений обработки middlewares результата роутинга.
+ * @author Egor Vasyakin <e.vasyakin@itevas.ru>
+ * @since 1.0
+ */
+class RoutingResultHandleMiddlewareException extends RoutingResultHandleException
+{}
