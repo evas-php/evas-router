20e6563bafa682a3b0e5eaf4e673c0e2c9e8d483
Egor Vasyakin <evasyakin@mail.ru>
2020-02-01 23:28:52 +0700

    Create AppRouterTrait.php

---
+++ src/Integrate/AppRouterTrait.php
@@ -0,0 +1,54 @@
+<?php
+/**
+ * @package evas-php/evas-router
+ */
+namespace Evas\Router\Integrate;
+
+use Evas\Router\Router;
+
+/**
+ * Константы для свойств трейта по умолчанию.
+ */
+if (!defined('EVAS_ROUTER_CLASS')) define('EVAS_ROUTER_CLASS', Router::class);
+
+/**
+ * Расширение класса приложения поддержкой router.
+ * @author Egor Vasyakin <e.vasyakin@itevas.ru>
+ * @since 1.0
+ */
+trait AppRouterTrait
+{
+    /**
+     * @var string имя класса router
+     */
+    protected $routerClass = EVAS_ROUTER_CLASS;
+
+    /**
+     * @var Router объект router
+     */
+    protected $router;
+
+    /**
+     * Установка имени класса router.
+     * @param string
+     * @return self
+     */
+    public static function setRouterClass(string $routerClass)
+    {
+        return static::instanceSet('routerClass', $routerClass);
+    }
+
+    /**
+     * Получение объекта router.
+     * @return Router
+     */
+    public static function router()
+    {
+        if (!static::instanceHas('router')) {
+            $routerClass = static::instanceGet('routerClass');
+            $router = new $routerClass;
+            static::instanceSet('router', $router);
+        }
+        return static::instanceGet('router');
+    }
+}
