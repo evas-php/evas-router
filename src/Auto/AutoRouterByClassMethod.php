c74d095904fdb744d995eb74ab926803db66d722
Egor Vasyakin <evasyakin@mail.ru>
2020-02-01 23:18:03 +0700

    Create AutoRouterByClassMethod.php

---
+++ src/Auto/AutoRouterByClassMethod.php
@@ -0,0 +1,107 @@
+<?php
+/**
+ * @package evas-php/evas-router
+ */
+namespace Evas\Router\Auto;
+
+use Evas\Router\Auto\BaseAutoRouter;
+
+/**
+ * Константы для свойств класса по умолчанию.
+ */
+if (!defined('EVAS_AUTOROUTER_CLASS_PREFIX')) define('EVAS_AUTOROUTER_CLASS_PREFIX', '');
+if (!defined('EVAS_AUTOROUTER_CLASS_POSTFIX')) define('EVAS_AUTOROUTER_CLASS_POSTFIX', '');
+
+if (!defined('EVAS_AUTOROUTER_METHOD_PREFIX')) define('EVAS_AUTOROUTER_METHOD_PREFIX', '');
+if (!defined('EVAS_AUTOROUTER_METHOD_POSTFIX')) define('EVAS_AUTOROUTER_METHOD_POSTFIX', '');
+
+/**
+ * Автороутер по классу.
+ * @author Egor Vasyakin <e.vasyakin@itevas.ru>
+ * @since 1.0
+ */
+class AutoRouterByClassMethod extends BaseAutoRouter
+{
+    /**
+     * @var string префикс класса
+     */
+    public $classPrefix = EVAS_AUTOROUTER_CLASS_PREFIX;
+
+    /**
+     * @var string постфикс класса
+     */
+    public $classPostfix = EVAS_AUTOROUTER_CLASS_POSTFIX;
+
+    /**
+     * @var string префикс метода
+     */
+    public $methodPrefix = EVAS_AUTOROUTER_METHOD_PREFIX;
+
+    /**
+     * @var string постфикс метода
+     */
+    public $methodPostfix = EVAS_AUTOROUTER_METHOD_POSTFIX;
+
+    /**
+     * Установка/сброс префикса класса.
+     * @param string|null
+     * @return self
+     */
+    public function classPrefix(string $value = null)
+    {
+        $this->classPrefix = $value;
+        return $this;
+    }
+
+    /**
+     * Установка/сброс постфикса класса.
+     * @param string|null
+     * @return self
+     */
+    public function classPostfix(string $value = null)
+    {
+        $this->classPostfix = $value;
+        return $this;
+    }
+
+    /**
+     * Установка/сброс префикса метода.
+     * @param string|null
+     * @return self
+     */
+    public function methodPrefix(string $value = null)
+    {
+        $this->methodPrefix = $value;
+        return $this;
+    }
+
+    /**
+     * Установка/сброс постфикса метода.
+     * @param string|null
+     * @return self
+     */
+    public function methodPostfix(string $value = null)
+    {
+        $this->methodPostfix = $value;
+        return $this;
+    }
+
+    /**
+     * Генерация обработчика сгенерированный класс => сгенерированный extends BaseAutoRouter метод.
+     * @param string путь
+     * @return array [class => method]
+     */
+    public function generateHandler(string $path): array
+    {
+        $parts = explode('/', $path);
+        array_shift($parts);
+        if (count($parts) < 2) array_unshift($parts, 'Index');
+        foreach ($parts as &$part) {
+            if (empty($part)) $part = 'index';
+            $part = ucfirst($part);
+        }
+        $class = $this->classPrefix . implode('\\', $parts) . $this->classPostfix;
+        $method = $this->methodPrefix . lcfirst(array_pop($parts)) . $this->methodPostfix;
+        return [$class => $method];
+    }
+}
