de1a70905902541c601bf29a261b24e62681cf74
Egor Vasyakin <evasyakin@mail.ru>
2020-02-01 23:16:17 +0700

    Create RouterException.php

---
+++ src/RouterException.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * @package evas-php/evas-router
+ */
+namespace Evas\Router;
+
+use Evas\Base\BaseException;
+
+/**
+ * Класс исключений роутера.
+ * @author Egor Vasyakin <e.vasyakin@itevas.ru>
+ * @since 1.0
+ */
+class RouterException extends BaseException
+{}
