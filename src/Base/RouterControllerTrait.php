a6368ed34d74b0efb46928dda9e4c38cbc2154c4
Egor Vasyakin <e.vasyakin@itevas.ru>
2020-04-30 05:00:58 +0700

    Update RouterControllerTrait.php
    Move code from Mvc\InitControllerTrait

--- src/Base/RouterControllerTrait.php
+++ src/Base/RouterControllerTrait.php
@@ -4,7 +4,13 @@
  */
 namespace Evas\Router\Base;
 
-use Evas\Mvc\InitControllerTrait;
+use Evas\Base\Exception\FileNotFoundException;
+use Evas\Router\Controller;
+
+/**
+ * Константы для свойств трейта по умолчанию.
+ */
+if (!defined('EVAS_CONTROLLER_CLASS')) define('EVAS_CONTROLLER_CLASS', Controller::class);
 
 /**
  * Трейт подключения контроллер-класса резултьта роутера.
@@ -14,7 +20,45 @@ use Evas\Mvc\InitControllerTrait;
 trait RouterControllerTrait
 {
     /**
-     * Подключаем расширение инициализации класса контроллера.
+     * @var string имя класса контроллера
      */
-    use InitControllerTrait;
+    public $controllerClass = EVAS_CONTROLLER_CLASS;
+
+    /**
+     * Установка имени класса контроллера.
+     * @param string имя класса
+     * @return self
+     */
+    public function controllerClass(string $controllerClass)
+    {
+        $this->controllerClass = &$controllerClass;
+        return $this;
+    }
+
+    /**
+     * Получение имени класса контроллера.
+     * @return string|null
+     */
+    public function getControllerClass(): ?string
+    {
+        return $this->controllerClass;
+    }
+
+    /**
+     * Создание экземпляра класса контроллера.
+     * @param string|null имя класса
+     * @param array|null аргументы конструктора
+     * @throws FileNotFoundException
+     * @return object
+     */
+    public function newController(string $controllerClass = null, array $args = null): object
+    {
+        if (empty($controllerClass)) {
+            $controllerClass = $this->getControllerClass();
+        }
+        if (!class_exists($controllerClass, true)) {
+            throw new FileNotFoundException("Controller class \"$controllerClass\" not found");
+        }
+        return new $controllerClass($args);
+    }
 }
