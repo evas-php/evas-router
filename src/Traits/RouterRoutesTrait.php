f9c9d7fca10a252f1f282317d97c2811dd08ff50
Egor Vasyakin <e.vasyakin@itevas.ru>
2021-07-22 06:13:59 +0300

    Create RouterRoutesTrait.php

---
+++ src/Traits/RouterRoutesTrait.php
@@ -0,0 +1,85 @@
+<?php
+/**
+ * Расширение роутера для работы с маппингом маршрутов.
+ * @package evas-php\evas-router
+ * @author Egor Vasyakin <egor@evas-php.com>
+ */
+namespace Evas\Router\Traits;
+
+use \InvalidArgumentException;
+use Evas\Base\Help\PhpHelper;
+use Evas\Router\Interfaces\RouterInterface;
+
+trait RouterRoutesTrait
+{
+    /** @var array маппинг обработчиков по методу и пути */
+    protected $routes = [];
+
+    /**
+     * Проверка типа обработчика на корректность типа данных.
+     * @param mixed обработчик
+     * @return bool
+     */
+    public function isCorrectHandlerType(&$handler): bool
+    {
+        return is_array($handler) || is_string($handler) || is_callable($handler);
+    }
+
+    /**
+     * Установка маршрута.
+     * @param string метод
+     * @param string путь
+     * @param mixed обработчик
+     * @return RouterInterface
+     * @throws InvalidArgumentException
+     */
+    public function route(string $method, string $path, $handler): RouterInterface
+    {
+        $method = strtoupper($method);
+        if (!$this->isCorrectHandlerType($handler) && !$handler instanceof RouterInterface) {
+            throw new InvalidArgumentException(sprintf(
+                'Argument 3 $handler must be a string or an array 
+                or a \Closure or an instance of $s, %s given',
+                RouterInterface::class,
+                PhpHelper::getType($handler)
+            ));
+        }
+        $this->routes[$method][$path] = $handler;
+        return $this;
+    }
+
+    /**
+     * Установка маршрута с несколькими методами.
+     * @param array методы
+     * @param string путь
+     * @param mixed обработчик
+     * @return RouterInterface
+     */
+    public function mergeRoute(array $methods, string $path, $handler): RouterInterface
+    {
+        foreach ($methods as &$method) {
+            $this->route($method, $path, $handler);
+        }
+        return $this;
+    }
+
+    /**
+     * Получение маршрутов сгруппированных по методам.
+     * @return array
+     */
+    public function getRoutes(): array
+    {
+        return $this->routes;
+    }
+
+    /**
+     * Получение группы маршрутов по методу вместе с методом ALL.
+     * @param string метод
+     * @return array
+     */
+    public function getRoutesByMethodWithAll(string $method): array
+    {
+        $method = strtoupper($method);
+        return array_merge($this->routes['ALL'] ?? [], $this->routes[$method] ?? []);
+    }
+}
