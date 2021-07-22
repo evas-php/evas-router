b2997fa8f9278b4d2c2bc63f4889ef57356900f5
Egor Vasyakin <e.vasyakin@itevas.ru>
2021-07-22 06:15:04 +0300

    Create AbstractAutoRouter.php

---
+++ src/Routers/AbstractAutoRouter.php
@@ -0,0 +1,37 @@
+<?php
+/**
+ * Абстрактный базовый класс автороутера.
+ * @package evas-php\evas-router
+ * @author Egor Vasyakin <egor@evas-php.com>
+ */
+namespace Evas\Router\Routers;
+
+use Evas\Router\Exceptions\RouterResultException;
+use Evas\Router\Interfaces\RouterResultInterface;
+use Evas\Router\Routers\MapRouter;
+
+abstract class AbstractAutoRouter extends MapRouter
+{
+    /**
+     * Абстрактная генерация обработчика маршрута.
+     * @param string путь
+     * @return mixed
+     */
+    abstract public function generateHandler(string $path);
+
+    /**
+     * Автороутинг.
+     * @param string путь
+     * @param array|null аргументы для обработчика
+     * @return RouterResultInterface
+     */
+    public function autoRouting(string $uri, array $args = null): ?RouterResultInterface
+    {
+        $handler = $this->generateHandler($uri);
+        try {
+            return $this->newResult($handler, $args);
+        } catch (RouterResultException $e) {
+            return null;
+        }
+    }
+}
