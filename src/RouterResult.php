36d153dab417161c1c4420d97a4d2ccd3e123ec0
Egor Vasyakin <e.vasyakin@itevas.ru>
2021-07-22 06:15:35 +0300

    Create RouterResult.php

---
+++ src/RouterResult.php
@@ -0,0 +1,205 @@
+<?php
+/**
+ * Класс результата роутинга.
+ * @package evas-php\evas-router
+ * @author Egor Vasyakin <egor@evas-php.com>
+ */
+namespace Evas\Router;
+
+use \Closure;
+use \Exception;
+use Evas\Base\App;
+use Evas\Base\Exceptions\FileNotFoundException;
+use Evas\Base\Help\PhpHelper;
+use Evas\Http\Interfaces\RequestInterface;
+use Evas\Router\Exceptions\RouterResultException;
+use Evas\Router\Interfaces\RouterResultInterface;
+use Evas\Router\Traits\RouterControllerTrait;
+
+class RouterResult implements RouterResultInterface
+{
+    /**
+     * Подключаем поддержку контроллера результата роутинга.
+     */
+    use RouterControllerTrait {
+        RouterControllerTrait::controllerClass as private _controllerClass;
+    }
+    
+    /** @var mixed обработчик */
+    public $handler;
+
+    /** @var array аргументы uri, разобранные в роутере */
+    public $args = [];
+
+    /** @var array middlewares */
+    public $middlewares;
+
+    /** @var array подготовленные обработчики */
+    protected $preparedHandlers;
+
+    /** @var array маппинг контроллеров для избежания дубликатов */
+    protected $controllersMap = [];
+
+    /**
+     * Конструктор.
+     * @param mixed|null обработчик
+     * @param array|null аргументы обработчика
+     * @param array|null middlewares
+     */
+    public function __construct($handler = null, array $args = null, array $middlewares = null)
+    {
+        $this->handler = &$handler;
+        if (!empty($args)) $this->args = &$args;
+        if (!empty($middlewares)) $this->middlewares = &$middlewares;
+    }
+
+    /**
+     * Переопределяем установку имени класса контроллера.
+     * @param string имя класса
+     * @return self
+     */
+    public function controllerClass(string $controllerClass): RouterResult
+    {
+        $this->controllerClass = &$controllerClass;
+        return $this;
+    }
+
+    /**
+     * Создание экземпляра класса контроллера.
+     * @param string|null имя класса контроллера
+     * @return object
+     * @throws FileNotFoundException
+     */
+    public function newController(string $controllerClass = null): object
+    {
+        if (empty($controllerClass)) {
+            $controllerClass = $this->getControllerClass();
+        }
+        if (!empty($this->controllersMap[$controllerClass])) {
+            return $this->controllersMap[$controllerClass];
+        }
+        if (!class_exists($controllerClass, true)) {
+            throw new FileNotFoundException("Controller class \"$controllerClass\" not found");
+        }
+        $controller = new $controllerClass($this->app, $this->request);
+        $this->controllersMap[$controllerClass] = &$controller;
+        return $controller;
+    }
+
+    /**
+     * Подготовка обработчика в виде пути к файлу.
+     * @param string путь к файлу
+     * @throws FileNotFoundException
+     */
+    protected function prepareFile(string &$handler)
+    {
+        $class = $this->newController();
+        if (!($class instanceof Controller)) {
+            throw new RouterResultException(sprintf(
+                'Controller class for file or \Closure must be 
+                instance or child of the %s, %s given',
+                Controller::class,
+                $this->getControllerClass()
+            ));
+        }
+        $class->throwIfNotCanView($handler);
+        $this->preparedHandlers[] = [[$class, 'view'], [$handler, $this->args]];
+    }
+
+    /**
+     * Подготовка обработчика в виде замыкания.
+     * @param Closure замыкание
+     */
+    protected function prepareClosure(Closure &$handler)
+    {
+        $class = $this->newController();
+        $method = $handler->bindTo($class);
+        $this->preparedHandlers[] = [$method, $this->args];
+    }
+
+    /**
+     * Подготовка обработчика в виде метода класса.
+     * @param array обработчик вида ['class' => 'method']
+     * @throws FileNotFoundException
+     * @throws RouterResultException
+     */
+    protected function prepareClass(array &$handler)
+    {
+        foreach ($handler as $className => $method) {
+            if (!class_exists($className, true)) {
+                throw new FileNotFoundException("Not found class $className");
+            }
+            $class = $this->newController($className);
+            if (!method_exists($class, $method)) {
+                throw new RouterResultException("Class $className does not contain method $method");
+                
+            }
+            $this->preparedHandlers[] = [[$class, $method], $this->args];
+            return;
+        }
+    }
+
+    /**
+     * Подготовка единичного обработчика.
+     * @param mixed обработчик
+     */
+    protected function prepareSingle(&$handler)
+    {
+        if ($handler instanceof \Closure) $this->prepareClosure($handler);
+        else if (is_string($handler)) $this->prepareFile($handler);
+        else if (PhpHelper::isAssoc($handler)) $this->prepareClass($handler);
+    }
+
+    /**
+     * Подготовка обработчика в виде списка обработчиков.
+     * @param array обработчики
+     */
+    protected function prepareList(array &$handlers)
+    {
+        foreach ($handlers as &$handler) {
+            $this->prepareSingle($handler);
+        }
+    }
+
+
+    /**
+     * Подготовка обработчика/обработчиков.
+     * @return self
+     * @throws RouterResultException
+     */
+    public function prepare()
+    {
+        $this->preparedHandlers = [];
+        if (!empty($this->middlewares)) {
+            try {
+                $this->prepareList($this->middlewares);
+            } catch (Exception $e) {
+                $message = 'The route middleware could not be resolved: '. $e->getMessage();
+                throw new RouterResultException($message, $e->getCode(), $e->getPrevious());
+            }
+        }
+        try {
+            if (PhpHelper::isNumericArray($this->handler)) $this->prepareList($this->handler);
+            else $this->prepareSingle($this->handler);
+        } catch (Exception $e) {
+            $message = 'The route handler could not be resolved: '. $e->getMessage();
+            throw new RouterResultException($message, $e->getCode(), $e->getPrevious());
+        }
+        return $this;
+    }
+
+
+    /**
+     * Вызов обработчика маршрута.
+     */
+    public function resolve()
+    {
+        // подготовка обработчика/обработчиков
+        if (!is_array($this->preparedHandlers)) $this->prepare();
+        // запуск обработчика
+        foreach ($this->preparedHandlers as &$handler) {
+            list($method, $args) = $handler;
+            if (false === call_user_func_array($method, $args)) break;
+        }
+    }
+}
