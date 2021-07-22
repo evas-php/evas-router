4a723e6512e26ad85ba83e79bc7af8feea7cd71f
Egor Vasyakin <e.vasyakin@itevas.ru>
2021-07-22 06:15:56 +0300

    Create RouterResultInterface.php

---
+++ src/Interfaces/RouterResultInterface.php
@@ -0,0 +1,23 @@
+<?php
+/**
+ * Интерфейс результата роутинга.
+ * @package evas-php\evas-router
+ * @author Egor Vasyakin <egor@evas-php.com>
+ */
+namespace Evas\Router\Interfaces;
+
+interface RouterResultInterface
+{
+    /**
+     * Конструктор.
+     * @param mixed|null обработчик
+     * @param array|null аргументы обработчика
+     * @param array|null middlewares
+     */
+    public function __construct($handler = null, array $args = null, array $middlewares = null);
+
+    /**
+     * Вызов обработчика маршрута.
+     */
+    // public function resolve();
+}
