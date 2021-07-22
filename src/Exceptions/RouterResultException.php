d9471ead94124acab54df284ebe35a7c90cddb1a
Egor Vasyakin <e.vasyakin@itevas.ru>
2021-07-22 06:16:00 +0300

    Create RouterResultException.php

---
+++ src/Exceptions/RouterResultException.php
@@ -0,0 +1,12 @@
+<?php
+/**
+ * Класс исключений обработчика результата роутинга.
+ * @package evas-php\evas-router
+ * @author Egor Vasyakin <egor@evas-php.com>
+ */
+namespace Evas\Router\Exceptions;
+
+use Evas\Router\Exceptions\RouterException;
+
+class RouterResultException extends RouterException
+{}
