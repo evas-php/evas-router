8cdc333366d40abbb8989170f2c2b5b2a0815041
Egor Vasyakin <e.vasyakin@itevas.ru>
2021-07-29 11:02:35 +0300

    Create tests help action classes

---
+++ tests/help/auth/LoginAction.php
@@ -0,0 +1,13 @@
+<?php
+/**
+ * Тестовый экшен входа.
+ */
+namespace Evas\Router\tests\help\auth;
+
+class LoginAction
+{
+    public function auto()
+    {
+        return 'user login';
+    }
+}
---
+++ tests/help/auth/RegistrationAction.php
@@ -0,0 +1,13 @@
+<?php
+/**
+ * Тестовый экшен егистрации.
+ */
+namespace Evas\Router\tests\help\auth;
+
+class RegistrationAction
+{
+    public function auto()
+    {
+        return 'user registration';
+    }
+}
