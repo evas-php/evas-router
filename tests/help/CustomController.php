4d0899af8b50ebd3683b3951707f4ef4b4b86e19
Egor Vasyakin <e.vasyakin@itevas.ru>
2021-07-29 11:15:22 +0300

    Create CustomController.php

---
+++ tests/help/CustomController.php
@@ -0,0 +1,10 @@
+<?php
+/**
+ * Тестовый кастомный контроллер файлов и функций.
+ */
+namespace Evas\Router\tests\help;
+
+use Evas\Router\Controller;
+
+class CustomController extends Controller
+{}
